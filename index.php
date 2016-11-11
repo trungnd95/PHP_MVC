97b9ae0db4f3965e1d683ebdffb6e01a761e7a89
Trung Nguyen <ndt8895@gmail.com>
2016-11-11 20:24:45 +0700

    Push project

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'config.php';
+require 'util/Auth.php';
+
+// Also spl_autoload_register (Take a look at it if you like)
+function __autoload($class) {
+    require LIBS . $class .".php";
+}
+
+
+// Load the Bootstrap!
+$bootstrap = new Bootstrap();
+$bootstrap->init();
